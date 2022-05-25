b937d8a62450a04af9e840bff391687632666de0
Andrew Hill <91089129+hill3325@users.noreply.github.com>
2022-05-25 16:24:07 -0400

    Add files via upload
    Index.cshtml in the Home controller has changes related to the setup

---
+++ CategoriesController.cs
@@ -0,0 +1,98 @@
+﻿using COMP_2084___Lab_2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP_2084___Lab_2.Controllers
+{
+
+    public class CategoriesController : Controller
+    {
+        static List<CategoriesModel> CategoriesList = new List<CategoriesModel>
+        {
+            new CategoriesModel{Id = 1, Name = "Food"},
+            new CategoriesModel{Id = 2, Name = "Tech"},
+            new CategoriesModel{Id = 3, Name = "Sports"}
+            };
+    
+        // GET: CategoriesController
+        public ActionResult Index()
+        {
+            return View(CategoriesList);
+        }
+
+        // GET: CategoriesController/Details/5
+        public ActionResult Details(int id)
+        {
+            var selected = CategoriesList.First<CategoriesModel>(x => x.Id == id);
+            return View(selected);
+        }
+
+        // GET: CategoriesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
---
+++ CategoriesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP_2084___Lab_2.Models
+{
+    public class CategoriesModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
